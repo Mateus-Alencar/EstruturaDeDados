d41850e9deb71206c43e435dfd4d18d8afb57d5a
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-04-07 20:52:41 -0300

    Update Fila_Ex1.py

--- Fila_Ex1.py
+++ Fila_Ex1.py
@@ -1,10 +1,8 @@
 def inserir(f, v):
     f.append(v)  # insere um valor no final da lista
 
-
 def retirar(f):
     return f.pop(0)  # retira e retorna um valor do início da lista
-
 
 def vazia_fila(f):
     return False if f else True
@@ -20,7 +18,6 @@ def totalElementos(f):
         item = retirar(aux)
         inserir(f, item)
     return contador
-    
 
 def mostrarConteudo(fila):
     aux = []
@@ -44,7 +41,31 @@ def verificarItem(fila, item):
         inserir(fila, item)
         
     return flag
-
+def intercalarFilas(fila1, fila2):
+    aux1 = []
+    aux2 = []
+    filaIntercalada = []
+    while not vazia_fila(fila1):
+        num = retirar(fila1)
+        inserir(filaIntercalada, num)
+        inserir(aux1, num)
+        while not vazia_fila(fila2):
+            num2 = retirar(fila2)
+            inserir(filaIntercalada, num2)
+            inserir(aux2, num2)
+            break
+    while not vazia_fila(fila2):
+            num2 = retirar(fila2)
+            inserir(filaIntercalada, num2)
+            inserir(aux2, num2)
+    while not vazia_fila(aux1):
+        item = retirar(aux1)
+        inserir(fila1, item)
+    while not vazia_fila(aux2):
+        item = retirar(aux2)
+        inserir(fila2, item)
+    return filaIntercalada
+        
 fila = []
 inserir(fila, 10)
 inserir(fila, 20)
@@ -53,4 +74,10 @@ inserir(fila, 30)
 mostrarConteudo(fila)
 print(verificarItem(fila, 20))
 print(totalElementos(fila))
+
+fila1 = [1,2,3,11]
+fila2 = [4,5,6]
+print(intercalarFilas(fila1, fila2))
+print(fila1)
+print(fila2)
 
