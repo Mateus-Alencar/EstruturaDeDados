7b0137cf8469fe9a481a2c6fa0f81a24d2a59b87
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-08-26 20:54:34 -0300

    Update SomaVetores.py

--- Exercicios/SomaVetores.py
+++ Exercicios/SomaVetores.py
@@ -14,4 +14,24 @@ def somaVetorNumPositivos(vetor, numElem):
         else:
             return somaVetorNumPositivos(vetor, numElem-1)
             
-print(somaVetor([1,2,3,4], 4))
+def encontrarMaiorElementoVetor(vetor, numElem):
+    if numElem == 1:
+        return vetor[0]
+    else:
+        valor = encontrarMaiorElementoVetor(vetor, numElem-1)
+        if valor > vetor[numElem-1]:
+            return valor
+        else: 
+            return vetor[numElem-1]
+
+
+def encontrarPosicaoMaiorValorVetor(vetor, numElem):
+    if numElem == 1:
+        return vetor[0]
+    else:
+        valor = encontrarPosicaoMaiorValorVetor(vetor, numElem-1)
+        if valor<vetor[numElem-1]:
+            return numElem
+        else: 
+            return numElem-1
+print(encontrarPosicaoMaiorValorVetor([1,2,3,-4], 4))
