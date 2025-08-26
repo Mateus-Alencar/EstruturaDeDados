4eaa687e960e738c9d70651b1d75267132c0c5dd
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-08-26 19:31:06 -0300

    Create multiplicacaoRussa.py

---
+++ Exercicios/multiplicacaoRussa.py
@@ -0,0 +1,9 @@
+def multiplicacaoRussa(n,x):
+    if n==0:
+        return 0
+    if n%2!=0:
+        return x + multiplicacaoRussa(n//2, x*2)
+    else:
+        return multiplicacaoRussa(n//2, x*2)
+        
+print(multiplicacaoRussa(42,35))
