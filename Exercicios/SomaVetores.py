6078a0e1bd5c78a86feb4fc7e48267879d0ad9e1
Mateus Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2025-08-26 20:16:06 -0300

    Update SomaVetores.py

--- Exercicios/SomaVetores.py
+++ Exercicios/SomaVetores.py
@@ -5,5 +5,13 @@ def somaVetor(vetor, numElem):
     else:
         return somaVetor(vetor,numElem-1) + vetor[numElem-1]
 
-    
+def somaVetorNumPositivos(vetor, numElem):
+    if numElem == 0:
+        return 0
+    else:
+        if vetor[numElem-1] > 0:
+            return somaVetorNumPositivos(vetor,numElem-1) + vetor[numElem-1]
+        else:
+            return somaVetorNumPositivos(vetor, numElem-1)
+            
 print(somaVetor([1,2,3,4], 4))
