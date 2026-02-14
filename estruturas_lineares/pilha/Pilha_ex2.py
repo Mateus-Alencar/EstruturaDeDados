b728b9b197afc166394427cea0446a7ce0fc0940
Mateus-Alencar <mateus.alencar3182@outlook.com>
2026-02-14 11:02:45 -0300

    Refatorando código

--- estruturas_lineares/pilha/Pilha_ex2.py
+++ estruturas_lineares/pilha/Pilha_ex2.py
@@ -3,21 +3,17 @@ class Pilha2:
         self.itens = []
 
     def is_vazia(self):
-        # Verifica se a Pilha está vazia
         return len(self.itens) == 0
 
     def enfileirar(self, item):
-        # Adiciona um item no final da Pilha (***PUSH***)
         self.itens.append(item)
         print(f'Item {item} enfileirado. Fila: {self.itens}')
 
     def desenfileirar(self):
-        # Remove o item da frente da Pilha (primeiro item) (***POP***)
         if self.is_vazia():
             print("A fila está vazia. Não é possível desenfileirar.")
             return None
         else:
-            # O método pop(0) remove o primeiro item da Pilha (frente da fila)
             item = self.itens.pop(0)
             print(f'Item {item} desenfileirado. Fila: {self.itens}')
             return item
@@ -48,13 +44,11 @@ class Pilha2:
 
 
     def imprimir_Pilha(self):
-        # Imprime os elementos da Pilha
         if self.is_vazia():
             print("A Pilha está vazia.")
         else:
             print(f'Pilha: {self.itens}')
 
-# Exemplo de uso da fila
 Pilha = Pilha2()
 
 Pilha.enfileirar(10)  
@@ -64,27 +58,20 @@ Pilha.enfileirar(40)
 Pilha.enfileirar(80)  
 Pilha.enfileirar(70)  
 
-# Ver a frente da fila
 print(f'Frente da fila: {Pilha.frente()}')
 
-# Desenfileirar um item
-Pilha.desenfileirar()  # Remove o item da frente (10)
+Pilha.desenfileirar()
 
-# Imprimir o estado atual da fila
 Pilha.imprimir_Pilha()
 
-# Verificar o tamanho da fila
 print(f'Tamanho da fila: {Pilha.tamanho()}')
 
-# Desenfileirar todos os itens
-Pilha.desenfileirar()  # Remove 20
-Pilha.desenfileirar()  # Remove 30
-Pilha.desenfileirar()  # Tenta remover de uma fila vazia
+Pilha.desenfileirar()
+Pilha.desenfileirar()
+Pilha.desenfileirar()
 
-# Verificar o tamanho da fila
 print(f'Tamanho da fila: {Pilha.tamanho()}')
 
-# Imprimir o estado da fila novamente
 Pilha.imprimir_Pilha()
 
 Pilha.desenfileirar()
@@ -92,5 +79,4 @@ Pilha.desenfileirar()
 
 Pilha.imprimir_Pilha()
 
-# Verificar o tamanho da fila
 print(f'Tamanho da fila: {Pilha.tamanho()}')
